485d9e83d8e295cd5ae0c60d36ff23c428485026
N.S.Devaraj <nsdevaraj@gmail.com>
2020-01-07 16:01:08 +0530

    reorg images

--- sidebars.js
+++ sidebars.js
@@ -1,19 +1,28 @@
-/**
- * Copyright (c) 2017-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-
 module.exports = {
-  sideBar: {
-     "Quick Start": [
-      "general/Getting-Started",
-      "general/Features",
-      "general/Copyright"
-    ],"Multiple Axes Chart": [
-      "multipleaxis/MultipleAxesChart"
-    ],"Horizon Chart": [
-      "horizon/HorizonChart"
-    ]  }
+    sideBar: {
+        "Quick Start": [
+            "general/Getting-Started",
+            "general/Features",
+            "general/Copyright"
+        ], "Visuals": [
+            "visuals/MultipleAxesChart",
+            "visuals/HorizonChart",
+            "visuals/MarimekkoChart",
+            "visuals/VarianceChart",
+            "visuals/ParallelCoordinates"
+        ], "Special Visuals": [
+            "special/BulletChart",
+            "special/GanttChart"
+        ], "Gauges": [
+            "gauges/LinearGauge",
+            "gauges/AdvancedGauge"
+        ], "Utilities": [
+            "utils/HierarchyTree",
+            "utils/TagCloud"
+        ], "Filters": [
+            "filters/HierarchicalFilter"
+        ], "Tables": [
+            "tables/HierarchyVarianceTable"
+        ]
+    }
 };
